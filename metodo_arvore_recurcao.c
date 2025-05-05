08554d6b33fa077a6e034b2755a91f1d130de7a7
Marina Cantarelli <111306797+MariCantarelli@users.noreply.github.com>
2025-05-05 20:49:15 +0000

    metodo arvore

--- metodo_arvore_recurcao.c
+++ metodo_arvore_recurcao.c
@@ -4,3 +4,12 @@ void f(int n){
         f(n/2);
     }
 }
+
+void f(int n){
+    if (n > 0) {
+        for (int i = 0; i < n; i++){
+            printf("%d\n", n);
+            f(n-1);
+        }
+    }
+}
